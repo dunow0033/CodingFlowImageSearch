e2ed0cdc8c148a148a1904e99c66a309934a94cd
Brian Donnoe <dunow33@hotmail.com>
2021-11-14 09:10:45 -0500

    did navigation file, video 3

--- app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.codinginflow.imagesearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codinginflow.imagesearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
