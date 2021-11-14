c57c640aa04ee428c7117c9dafcc5117176f32ed
Brian Donnoe <dunow33@hotmail.com>
2021-11-14 14:35:49 -0500

    finishing up retrofit, video 5

--- app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.codinginflow.imagesearchapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codinginflow.imagesearchapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
